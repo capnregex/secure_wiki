ddf6983b0cc41ba4ed84fc0dd9f187c7073bd85c
Robert Ferney <capnregex@gmail.com>
2011-11-01 20:26:25 -0600

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecureWiki::Application.config.secret_token = '3afb0df19a0bee44d3a8484da3355253896cec9bd5a2ae35c3c71db62ec7ed03f52efa341ff35d1f29aaba8d869eb9f07463c656cc84e08870cc1aea665e0f59'
